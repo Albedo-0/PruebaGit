0d40dd3818085655d61ec07ee6aafd3339166929
sergi <sergi@DESKTOP-LSEV2JM>
2024-03-11 20:53:45 +0100

    V1.1 Cambio mensaje system.out

--- ProyectoGit/src/paquete1/Principal.java
+++ ProyectoGit/src/paquete1/Principal.java
@@ -3,7 +3,7 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("probando repositorios Git en º DAM");
+		System.out.println("probando repositorios Git");
 
 	}
 
