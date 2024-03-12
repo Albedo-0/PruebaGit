0e1482b695d00a72a996007641c89009955d4fef
sergi <sergi@DESKTOP-LSEV2JM>
2024-03-12 16:04:44 +0100

    V3:new System hola

--- ProyectoGit/src/paquete1/Principal.java
+++ ProyectoGit/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("probando repositorios Git");
 		System.out.println("Adios");
+		System.out.println("hola");
 
 	}
 
